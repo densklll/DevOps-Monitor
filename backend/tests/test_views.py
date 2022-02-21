bb6b8dcc5712a7e6cace273f260f202853a5e586
densklll <wqjasabim@mail.ru>
2022-02-22 01:42:10 +0300

    test(api): Mark pytest isolation for pipeline models (local dev)

--- backend/tests/test_views.py
+++ backend/tests/test_views.py
@@ -22,3 +22,5 @@ class PipelineAPITest(TestCase):
 # 2022-01-20: Mark Compose dependency boot order (prod checklist)
 
 # 2022-02-06: Stub integration service timeout budget (prod checklist)
+
+# 2022-02-22: Mark pytest isolation for pipeline models (local dev)
