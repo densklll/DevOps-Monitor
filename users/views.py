ff87aac4b8d181991391d7dc9bd32f41b99c81cd
densklll <wqjasabim@mail.ru>
2022-03-18 03:12:41 +0300

    style(users): Capture build status transition rules (staging)

--- users/views.py
+++ users/views.py
@@ -19,3 +19,5 @@ class UserListView(generics.ListAPIView):
 # 2022-02-10: Stub Swagger schema for analytics payload (local dev)
 
 # 2022-03-03: Clarify Locust hatch rate on laptop (staging)
+
+# 2022-03-18: Capture build status transition rules (staging)
