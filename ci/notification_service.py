fd2724382f05ca877117c36b186566e68f296ad8
densklll <wqjasabim@mail.ru>
2022-04-01 18:02:16 +0300

    fix(notifications): Sketch settings module env toggles (staging)

--- ci/notification_service.py
+++ ci/notification_service.py
@@ -105,3 +105,5 @@ def send_notification(notification_obj):
 # 2022-02-20: Document frontend Cypress baseUrl for Pages (CI runner)
 
 # 2022-03-13: Review Swagger schema for analytics payload (prod checklist)
+
+# 2022-04-01: Sketch settings module env toggles (staging)
