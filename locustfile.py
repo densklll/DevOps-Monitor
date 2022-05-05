9c3e6a958c953f5be33a8f84f5339a4f66de926d
densklll <wqjasabim@mail.ru>
2022-05-05 03:18:29 +0300

    chore(locust): Adjust integration service timeout budget (prod checklist)

--- locustfile.py
+++ locustfile.py
@@ -23,3 +23,5 @@ class WebsiteUser(HttpUser):
 # 2022-03-17: Note frontend Cypress baseUrl for Pages (prod checklist)
 
 # 2022-04-09: Capture build status transition rules (CI runner)
+
+# 2022-05-05: Adjust integration service timeout budget (prod checklist)
